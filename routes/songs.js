c772c199606b6a5b66982b192022381b6d4b7627
LizzieDworsky <amylizzieroyal@gmail.com>
2024-01-29 14:56:58 -0700

    added song endpoints for basic CRUD functionality

--- routes/songs.js
+++ routes/songs.js
@@ -2,4 +2,44 @@ const express = require("express");
 const router = express.Router();
 const { Song, validateSong } = require("../models/Song");
 
+router.get("/", async (req, res) => {
+    try {
+        res.status(200).send("Get all Request.");
+    } catch (error) {
+        res.status(500).send(`Internal Server Error ${error}`);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        res.status(200).send("Get by ID Request.");
+    } catch (error) {
+        res.status(500).send(`Internal Server Error ${error}`);
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        res.status(201).send("Post Request.");
+    } catch (error) {
+        res.status(500).send(`Internal Server Error ${error}`);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        res.status(200).send("Put Request.");
+    } catch (error) {
+        res.status(500).send(`Internal Server Error ${error}`);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        res.status(204).send("Delete Request.");
+    } catch (error) {
+        res.status(500).send(`Internal Server Error ${error}`);
+    }
+});
+
 module.exports = router;
