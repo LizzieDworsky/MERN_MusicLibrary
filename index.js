730836ed79275f90c8a3fe983688d36290690b28
LizzieDworsky <amylizzieroyal@gmail.com>
2024-01-29 14:52:50 -0700

    set up route for songs

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const connectDb = require("./db/db");
 
+const songs = require("./routes/songs");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 connectDb();
+
+app.use("/api/songs", songs);
 
 const PORT = process.env.PORT || 5000;
 
