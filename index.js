91871af444ebd7dcde259ab30eba671d546b4094
LizzieDworsky <amylizzieroyal@gmail.com>
2024-01-29 14:21:25 -0700

    added database connection config

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDb = require("./db/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+connectDb();
 
 const PORT = process.env.PORT || 5000;
 
