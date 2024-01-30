ab8f92c1e0ad56a9425920940890f9c423009ada
LizzieDworsky <amylizzieroyal@gmail.com>
2024-01-29 18:47:18 -0700

    added framework logic for search bar functionality

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const SearchBar = ({}) => {
-    return <div>Search Bar</div>;
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(searchTerm);
+        setSearchTerm("");
+    };
+
+    return (
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button type="submit">Search</button>
+        </form>
+    );
 };
 
 export default SearchBar;
